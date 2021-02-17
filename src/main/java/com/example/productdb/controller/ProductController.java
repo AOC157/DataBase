b757820c8b4a5e2fc52926b025216c0d6a29db75
Hatef Zamany <hatef.zamany@yahoo.com>
2021-02-17 16:57:06 +0330

    get data from user

--- src/main/java/com/example/productdb/controller/ProductController.java
+++ src/main/java/com/example/productdb/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.example.productdb.controller;
 import com.example.productdb.model.Product;
 import com.example.productdb.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,50 +21,15 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    public void saveProduct(){
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            Product product = new Product();
-
-            System.out.print("enter name: ");
-            product.setName(scanner.nextLine());
-
-            System.out.print("enter color: ");
-            product.setColor(scanner.nextLine());
-
-            System.out.print("enter price: ");
-            product.setPrice(Double.parseDouble(scanner.nextLine()));
-
-            System.out.print("enter date of manufacture: ");
-            product.setDateOfManufacture(scanner.nextLine());
-
-            System.out.print("enter expiration date: ");
-            product.setExpirationDate(scanner.nextLine());
-
-            productRepository.save(product);
-
-            System.out.print("do you want to finish?(if yes press \"y\") ");
-            if (scanner.nextLine().charAt(0) == 'y'){
-                break;
-            }
-
-        }
+    public void saveProduct(Product product){
+        productRepository.save(product);
     }
 
 
-    @RequestMapping("/")
-    public String showProduct(){
-        saveProduct();
+    @RequestMapping(value = "/test/postmethod/saveProduct" , method = RequestMethod.POST , consumes = "application/json")
+    public String showProduct(@RequestBody Product product){
+        saveProduct(product);
 
-        List<Product> allProducts = productRepository.findAll();
-
-        StringBuilder builder = new StringBuilder();
-
-        for(Product p : allProducts){
-            builder.append(p.toString());
-            builder.append(System.lineSeparator());
-        }
-
-        return builder.toString();
+        return product.toString();
     }
 }
