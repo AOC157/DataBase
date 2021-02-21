d4172d6de65cde861e25f0bd3079ade65da22ae5
Hatef Zamany <hatef.zamany@yahoo.com>
2021-02-21 20:15:49 +0330

    add get request

--- src/main/java/com/example/productdb/controller/ProductController.java
+++ src/main/java/com/example/productdb/controller/ProductController.java
@@ -3,13 +3,8 @@ package com.example.productdb.controller;
 import com.example.productdb.model.Product;
 import com.example.productdb.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Scanner;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -17,19 +12,19 @@ public class ProductController {
     @Autowired
     public ProductRepository productRepository;
 
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
-    public void saveProduct(Product product){
+    @RequestMapping(value = "/" , method = RequestMethod.POST , consumes = "application/json")
+    public String productPostRequest(@RequestBody Product product){
         productRepository.save(product);
-    }
-
-
-    @RequestMapping(value = "/test/postmethod/saveProduct" , method = RequestMethod.POST , consumes = "application/json")
-    public String showProduct(@RequestBody Product product){
-        saveProduct(product);
 
         return product.toString();
+    }
+
+    @GetMapping(value = "/" ,produces = "application/json")
+    @ResponseBody
+    public HttpEntity<Product> productGetRequest(){
+        Product product = new Product("pen" , "red" , 5000.0 , "1399.09.30" , "");
+
+        return new HttpEntity<>(product);
     }
 }
