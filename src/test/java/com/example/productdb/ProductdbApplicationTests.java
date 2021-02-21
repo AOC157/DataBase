37e048627533e39564ec766b2b7a904ab4ddb3fd
Hatef Zamany <hatef.zamany@yahoo.com>
2021-02-21 20:17:07 +0330

    add unit test to check id value

--- src/test/java/com/example/productdb/ProductdbApplicationTests.java
+++ src/test/java/com/example/productdb/ProductdbApplicationTests.java
@@ -1,13 +1,54 @@
 package com.example.productdb;
 
+import com.example.productdb.controller.ProductController;
+import com.example.productdb.repository.ProductRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductController.class)
 class ProductdbApplicationTests {
+
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void productTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(0));
     }
 
 }
